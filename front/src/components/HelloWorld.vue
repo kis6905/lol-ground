06c4929cb46808eff90a9e756c5ece1ea4afb7fb
kis6905 <kis6905@gmail.com>
2022-10-27 21:25:02 +0900

    init front

---
+++ front/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-alert color="info" icon="$info">
+      Hello World
+    </v-alert>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    
+  }),
+}
+</script>
