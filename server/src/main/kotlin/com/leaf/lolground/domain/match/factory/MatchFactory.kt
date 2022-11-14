7bd43e7e965b229b2501bba885b65a2c411f86c5
kis6905 <kis6905@gmail.com>
2022-11-15 01:11:20 +0900

    코드정리

--- server/src/main/kotlin/com/leaf/lolground/domain/match/factory/MatchFactory.kt
+++ server/src/main/kotlin/com/leaf/lolground/domain/match/factory/MatchFactory.kt
@@ -1,18 +1,18 @@
 package com.leaf.lolground.domain.match.factory
 
 import com.leaf.lolground.domain.match.dto.RecentMatch
-import com.leaf.lolground.infrastructure.api.lol.match.dto.Match
 import com.leaf.lolground.infrastructure.api.lol.match.dto.MatchInfo
-import com.leaf.lolground.infrastructure.helper.*
+import com.leaf.lolground.infrastructure.helper.diffDaysFromNow
+import com.leaf.lolground.infrastructure.helper.diffHoursFromNow
+import com.leaf.lolground.infrastructure.helper.diffMinutesFromNow
+import com.leaf.lolground.infrastructure.helper.toLocalDateTime
 import org.springframework.stereotype.Component
-import java.time.LocalDateTime
 
 @Component
 class MatchFactory {
     fun createRecentMatch(matchInfo: MatchInfo, puuid: String): RecentMatch {
         val gameStartTime = matchInfo.gameStartTime.toLocalDateTime()
 
-        println("gameStartTime: ${gameStartTime.toString()}")
         val diffMinutes = gameStartTime.diffMinutesFromNow()
         val diffHours = gameStartTime.diffHoursFromNow()
         val playedAgo = if (diffMinutes < 60) {
